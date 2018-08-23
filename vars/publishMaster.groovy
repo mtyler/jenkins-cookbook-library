127421719a214cf4887a7b14e74ca8e4fe35ba1e
mtyler <michael.tyler@gmail.com>
2018-08-23 13:18:27 -0400

    Access Token functioning in init script. trying to fix Dockerfile location

--- vars/publishMaster.groovy
+++ vars/publishMaster.groovy
@@ -47,7 +47,8 @@ def call(String cookbookName){
         //filename 'Dockerfile'
         //no-worky "\"\${WORKSPACE}\"/Dockerfile" = /"${WORKSPACE}"/Dockerfile
         //no-worky "\${WORKSPACE}/Dockerfile" = /"${WORKSPACE}/Dockerfile
-        filename "\$WORKSPACE/Dockerfile"
+        //no-worky "\$WORKSPACE/Dockerfile" = /$WORKSPACE/Dockerfile
+        filename \"\$WORKSPACE\"/Dockerfile
         args "--net host \
               -u 0:0 \
               --dns 192.168.1.1 \
